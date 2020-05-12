898f13c5ed3af488828d8585c0b3bb55f461212f
Nathan <nateyl.b@gmail.com>
2020-05-12 12:42:56 -0700

    created table schema

--- migrations/20200512123321_car-dealer-table.js
+++ migrations/20200512123321_car-dealer-table.js
@@ -1,8 +1,18 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable("cars", tbl => {
+        // primary key, called id, autoincrements, not null
+        tbl.increments();
+        tbl.string("VIN", 17).notNullable().unique();
+        tbl.string("Make").notNullable();
+        tbl.string("Model").notNullable();
+        tbl.integer("Mileage").notNullable();
+        tbl.string("Transmission Type");
+        tbl.string("Title Status");
+        tbl.timestamps(true, true); // created_at and updated_at
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+return knex.schema.dropTableIfExists("cars")
 };
