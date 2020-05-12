1de5e94b6ba1fa738abfa75d76102869c369e62b
Nathan <nateyl.b@gmail.com>
2020-05-12 13:20:43 -0700

    POST working

--- server.js
+++ server.js
@@ -17,4 +17,16 @@ server.get("/", (req, res)=>{
     })
 })
 
+server.post("/", (req, res)=>{
+    db("Cars")
+    .insert(req.body)
+    .then(id=>{
+        res.status(201).json({data: id[0], message: "Succesfully created a new car."})
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: error.messsage });
+      });
+})
+
 module.exports = server;
