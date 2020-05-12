152682637477420acee5d875f491dd29338b80c6
Nathan <nateyl.b@gmail.com>
2020-05-12 13:16:26 -0700

    GET is working

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 
+const db = require("./data/dbConfig.js");
+
 const server = express();
 
 server.use(express.json());
+
+server.get("/", (req, res)=>{
+    db("Cars")
+    .then(cars =>{
+        res.status(200).json(cars)
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({message: err.message})
+    })
+})
 
 module.exports = server;
