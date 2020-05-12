d80b2bea9e608d82fb2be1c3ceb0d79de1ff263f
Nathan <nateyl.b@gmail.com>
2020-05-12 12:27:24 -0700

    .db3 created

--- knexfile.js
+++ knexfile.js
@@ -5,24 +5,10 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+      filename: './data/car-dealer.db3'
+    },
+    useNullAsDefault: true, // for SQLite only
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
 
   production: {
